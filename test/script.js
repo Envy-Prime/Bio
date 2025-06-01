d4aa76bcb721117f8c56a7a8ba69d5f44cb579cb
duyproft <duyproft@gmail.com>
2025-06-01 10:15:03 +0700

    Add files via upload

---
+++ test/script.js
@@ -0,0 +1,8 @@
+window.addEventListener('scroll', () => {
+  const video = document.querySelector('.video-container video');
+  const scrollY = window.scrollY;
+  const fadeStart = 0;    // bắt đầu mờ dần
+  const fadeEnd = 300;    // mờ hoàn toàn sau 300px
+  const opacity = 1 - Math.min(Math.max(scrollY - fadeStart, 0) / (fadeEnd - fadeStart), 1);
+  video.style.opacity = opacity;
+});
